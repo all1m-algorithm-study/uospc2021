5cd667d76c08533255c88b1d54ef27e026263d52
travelbeeee <sochun1518@naver.com>
2021-11-29 14:01:30 +0900

    bulb-easy, bulb-hard DataGenerator Update

---
+++ problems/bulb-easy/sources/Data_Generator.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include "testlib.h"
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	ios::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+
+	registerGen(argc, argv, 1);
+
+	int N = rnd.next(1, 100000);
+
+	cout << N << '\n';
+	for (int i = 0; i < N - 1; i++) {
+		cout << rnd.next(0, 1) << ' ';
+	}
+	cout << rnd.next(0, 1) << '\n';
+
+	return 0;
+}
